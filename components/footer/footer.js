9f7fb4bd5370569e0873ebdc7c9a660f5fb26a48
Kat Graham <kat.exe11@yahoo.com>
2024-03-13 19:55:43 -0400

    updating site with name changes

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -44,7 +44,7 @@ class Footer extends HTMLElement {
                     </div>
                     <div id="copyright">
                         <ul>
-                            <li>&copy; Kat Graham</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                            <li>&copy; Kat Terranova</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                             <li>Icons: <a href="https://www.flaticon.com/free-icons/computer" title="computer icons">Computer icons created by Freepik - Flaticon</a></li>
                             
                         </ul>
