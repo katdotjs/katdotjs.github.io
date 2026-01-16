1c2130b468de797ce17149c8132fcfdaa4970741
Kat Graham <kat.exe11@yahoo.com>
2026-01-15 21:08:31 -0500

    available for workies!

--- app/page.tsx
+++ app/page.tsx
@@ -123,8 +123,8 @@ export default function Home() {
 
                 <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-4 text-sm text-muted-foreground">
                   <div className="flex items-center gap-2 justify-start!">
-                    <div className="w-2 h-2 bg-red-500 rounded-full animate-pulse"></div>
-                    Not Available for work
+                    <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></div>
+                    Available for work
                   </div>
                   <div>Rochester, NY</div>
                 </div>
